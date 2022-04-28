2c71f43bb41a6de9745a042f3f2c7edd32356d94
wesleytw <wesliutw@gmail.com>
2022-04-29 01:48:13 +0800

    first-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{jsx,html,js}',
+    './src/components/**/*.{jsx,html,js}'
   ],
   theme: {
     extend: {},
